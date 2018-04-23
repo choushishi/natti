8b75748262b02d9f41e758dbfe9076a9a62b7e07
choushishi <choushishi@Elliott.local>
2018-04-23 18:44:29 +1000

    add time in logs

--- monitor.py
+++ monitor.py
@@ -3,6 +3,7 @@ import getpass
 from twilio.rest import Client
 import time
 import difflib
+import datetime
 
 # twilio configurations
 print("Please enter your twilio project account details: ")
@@ -13,27 +14,32 @@ to_nmb = getpass.getpass("to: ")
 client = Client(account_sid, auth_token)
 
 # monitor setup
+refresh = float(input("Entering refreshing rate in seconds: "))
 url = "https://www.naati.com.au/other-information/ccl-testing/"
 cached = urllib.request.urlopen(url).read().decode("utf-8").splitlines()
 cached = ["test"] + cached  # tesing twilio on first iteration
 starttime = time.time()
-refresh = 60.0
 
 # initialize differ
 d = difflib.Differ()
 
+
+def log(msg):
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "  " + msg)
+
+
 # main loop
 while True:
-    print("tick")
+    log("tick")
     latest = urllib.request.urlopen(url).read().decode("utf-8").splitlines()
     diff = d.compare(cached, latest)
     if cached != latest:
-        print("diff!")
+        log("diff!")
         diff = "\n".join([l for l in diff if l.startswith('+ ') or l.startswith('- ')])  # only send differences
         diff = diff[:130]  # limit sms body to 130 characters
-        print(diff)
-        print("sending message")
+        log(diff)
+        log("sending message")
         message = client.messages.create(to=to_nmb, from_=from_nmb, body=diff)
-        print("message sent")
+        log("message sent")
         cached = latest
     time.sleep(refresh - ((time.time() - starttime) % refresh))
